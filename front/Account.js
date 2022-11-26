6055e93c48266e340b78be57efd7e0321605d925
Addy Yang <yang.addy@hotmail.com>
2022-11-26 14:11:19 -0500

    Added account features

---
+++ front/Account.js
@@ -0,0 +1,35 @@
+var email="example@gmail.com";
+var password="placeholder";
+var censoredPassword;
+var passwordShown=false;
+
+window.onload = ()=> {
+    //fetch password from backend
+    //fetch email from backend
+    censorPassword();    
+    console.log("entered");
+    document.getElementById('email').innerHTML = email+"<br>";
+    document.getElementById('password').innerHTML = censoredPassword+"<br>";
+};
+
+function showHidePass() {
+    if (passwordShown) {
+        document.getElementById('showHidePass').innerHTML="Show Password";
+        document.getElementById('password').innerHTML=censoredPassword+"<br>";
+    } else {
+        document.getElementById('showHidePass').innerHTML="Hide Password";
+        document.getElementById('password').innerHTML=password+"<br>";
+    }
+    passwordShown=!passwordShown;
+}
+
+function censorPassword() {
+    censoredPassword=password.charAt(0);
+    for (let i=1; i<password.length; ++i) {
+        censoredPassword+="*";
+    }
+}
+
+function changePassword() {
+    
+}
